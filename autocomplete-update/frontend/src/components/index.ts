4c7140da200385c2f72f46c57a3a95d9927b923c
Flavio Troia <flavio.troia@overzoom.it>
2026-01-23 09:35:00 +0100

    fix: PASS postgres in env

--- autocomplete-update/frontend/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as PredictForm } from "./PredictForm";
-export { default as PlayerAutocomplete } from "./PlayerAutocomplete";
-export { default as ValueBetTable } from "./ValueBetTable";
-export { default as ValueBetsLive } from "./ValueBetsLive";
